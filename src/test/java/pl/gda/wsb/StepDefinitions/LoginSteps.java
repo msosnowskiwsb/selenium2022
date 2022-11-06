8cc0b73c4f049dd71ad0b9b582caa2699a6d42d1
Mateusz Sosnowski <msosnowski@wsb.gda.pl>
2022-11-06 12:02:52 +0100

    DriverFactory

--- src/test/java/pl/gda/wsb/StepDefinitions/LoginSteps.java
+++ src/test/java/pl/gda/wsb/StepDefinitions/LoginSteps.java
@@ -4,41 +4,39 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pl.gda.wsb.PageFactory.DriverFactory;
+import pl.gda.wsb.PageFactory.LoginPage;
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginSteps {
 
-    WebDriver driver = null;
+    WebDriver driver = DriverFactory.getChromeDriver();
+    LoginPage loginPage = new LoginPage(driver);
 
     @Given("Open login page")
     public void open_login_page(){
-        // Declaration and instantiation of driver
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-
+        driver = DriverFactory.getChromeDriver();
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
     }
 
     @When("User enters username and password")
     public void user_enters_username_and_password(){
-        driver.findElement(By.name("username")).sendKeys("Admin");
-        driver.findElement(By.name("password")).sendKeys("564564");
+        loginPage.enterUsername("Admin");
+        loginPage.enterPassword("Admin");
     }
 
     @And("Click on login button")
     public void click_on_login_button(){
-        driver.findElement(By.className("orangehrm-login-button")).click();
+        loginPage.clickOnLoginButton();
     }
 
     @Then("The validation message is displayed")
     public void the_validation_message_is_displayed() {
-        String validationMessage = driver.findElement(By.className("oxd-alert-content-text")).getText();
+        Assert.assertEquals("Invalid credentials",loginPage.getValidationMessage());
     }
 }
